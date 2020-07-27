1850472371604ce8c2daab14eab4398109843192
YeongBeom Heo <hybeom@gmail.com>
2020-07-27 21:59:41 +0900

    20200726-1

--- React_movie/first_app/src/App.js
+++ React_movie/first_app/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import Potato from "./Potato";
 
+function Food({ fav }){
+  return <h1>I Like {fav} </h1>;
+}
 
 function App() {
   return (
     <div>Say Hello!
-      <Potato />
+      <Food fav = "kimchi"/>
+      <Food fav = "Bab"/>
+      <Food fav = "samgiopsal"/>
+      <Food fav = "chukumi"/>
     </div> 
   );
 }
-
+// function App() {
+//   return (
+//     <div>Say Hello!
+//       <Food 
+//       fav = "kimchi"
+//       soemthing = {true}
+//       papapapa = {["hello", 1,2,3,4,true]}
+//        />
+//     </div> 
+//   );
+// }
 export default App;
