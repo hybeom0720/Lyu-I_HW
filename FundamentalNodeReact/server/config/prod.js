3ee1ed5e17d99c7c6153cf1e2645ff4bbed42a9e
hybeom0720 <hybeom@gmail.com>
2020-08-23 19:03:32 +0900

    React

---
+++ FundamentalNodeReact/server/config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://YeongBeom:pass123@boilerplate-shard-00-00.0ap8d.mongodb.net:27017,boilerplate-shard-00-01.0ap8d.mongodb.net:27017,boilerplate-shard-00-02.0ap8d.mongodb.net:27017/boilerplate?ssl=true&replicaSet=atlas-6hhfn6-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
