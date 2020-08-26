146baab3b6e5fe96a2000508864ceae6ad493e21
hybeom0720 <hybeom@gmail.com>
2020-08-26 22:50:46 +0900

    20200826

---
+++ FundamentalNodeReact/client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import {combinedReducers} from 'redux';
+// import user from "./user_reducer";
+
+const rootReducer = combinedReducers({
+    //user,
+})
+//여러개의 reducer를 하나로 합쳐주는
+export default rootReducer
